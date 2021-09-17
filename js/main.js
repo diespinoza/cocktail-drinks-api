4abfa75ae106bee7e755ad033cbed1e01d027f25
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-09-17 17:55:56 -0400

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -3,33 +3,32 @@ document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
     let drinkChoice = document.querySelector('input').value
-    //Check and modify the input to make sure it'll work
-    //Find all spaces and replace them with a single space using regex
-    let spaceRegExp = /\s+/g;
-    let oneSpace = drinkChoice.replace(spaceRegExp, " ")
-    //Remove leading space and trailing space using trim() method
-    drinkChoice = oneSpace.trim()
+    //Replace all spaces with a single space using regex, trim extra spaces
+    drinkChoice = drinkChoice.trim().replace(/\s+/g , " ")
     baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkChoice
+    //Create URL object
     let url = new URL(baseUrl)
-
-    console.log("The base url is " + baseUrl)
-    //url.search = drinkChoice
     console.log("The search parameter is " + url.search)
-    console.log(url)
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(data.drinks[0])
+            if(!data || data.length <= 0){
+                throw new Error("no data received")
+            }
+            console.log(data.drinks)
             document.querySelector('h2').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
             document.querySelector('h3').innerText = data.drinks[0].strInstructions
+
+
+
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
 }
 
-/* TODO: Handle drink names with a space
+/* TODO:
  *       mispellings
  *       a caroussel of whiskey  drinks. automatically rotating
  *
